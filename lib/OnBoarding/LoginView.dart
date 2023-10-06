cdd576ccfac2ef2c63efdb5304fda7c555ed52ec
Pablobel <pablo.beltran.reyfernando@gmail.com>
2023-10-06 21:27:04 +0200

    Usuario y contraseña

--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -3,8 +3,39 @@ import 'package:flutter/material.dart';
 class LoginView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    Column columna = Column(children: [
+      Text("Kyty Login",style: TextStyle(fontSize: 25)),
+
+      Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Usuario',
+          ),
+        ),
+      ),
+      Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Contraseña',
+          ),
+          obscureText: true,
+        ),
+      ),
+    ],);
+    AppBar appBar = AppBar(
+      title: const Text('Login'),
+      centerTitle: true,
+      shadowColor: Colors.black,
+      backgroundColor: Colors.blue,
+    );
+    Scaffold scaf=Scaffold(body: columna,
+      appBar: appBar,);
+
+    return scaf;
+
   }
 
 }
