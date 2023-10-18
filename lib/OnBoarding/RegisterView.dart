1463f9192e31156ccfb76c767bf3b7fdd9349702
Pablobel <pablo.beltran.reyfernando@gmail.com>
2023-10-18 20:37:14 +0200

    Página de registrar.

--- lib/OnBoarding/RegisterView.dart
+++ lib/OnBoarding/RegisterView.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
 class RegisterView extends StatelessWidget{
+  late BuildContext _context;
+  void onClickCancelar(){
+    Navigator.of(_context).pushNamed("/registerview");
+  }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    _context=context;
+    Column columna = Column(children: [
+      Text("Kyty Register",style: TextStyle(fontSize: 25)),
+
+      Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Usuario',
+          ),
+        ),
+      ),
+      Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Contraseña',
+          ),
+          obscureText: true,
+        ),
+      ),
+      Padding(padding: EdgeInsets.symmetric(horizontal: 60, vertical: 16),
+        child: TextFormField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Repetir Contraseña',
+          ),
+          obscureText: true,
+        ),
+      ),
+      Row(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(onPressed:(){}, child: Text("Aceptar"),),
+          TextButton(onPressed: onClickCancelar , child: Text("Cancelar"),)
+        ],)
+    ],);
+    AppBar appBar = AppBar(
+      title: const Text('Login'),
+      centerTitle: true,
+      shadowColor: Colors.black,
+      backgroundColor: Colors.blue,
+    );
+    Scaffold scaf=Scaffold(body: columna,
+      appBar: appBar,);
+
+    return scaf;
+
   }
+
 }
