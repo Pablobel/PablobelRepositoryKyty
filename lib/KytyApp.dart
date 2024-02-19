ec6d106b1e5a701e78760c99e1259f0d7f0a3d94
Pablobel <pablo.beltran.reyfernando@gmail.com>
2024-02-19 02:52:24 +0100

    creación de homeview

--- lib/KytyApp.dart
+++ lib/KytyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Main/HomeView.dart';
 import 'OnBoarding/LoginView.dart';
 import 'OnBoarding/RegisterView.dart';
 class KytyApp extends StatelessWidget{
@@ -10,6 +11,7 @@ class KytyApp extends StatelessWidget{
       routes: {
         '/loginview':(context) => LoginView(),
         '/registerview':(context) => RegisterView(),
+        '/homeview':(context)=> HomeView(),
       },
       initialRoute: '/loginview',
     );
--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget{
@@ -12,18 +14,17 @@ class LoginView extends StatelessWidget{
 
   void onClickAceptar() async {
     try {
-      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: emailAddress,
-        password: password,
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailAddress,
+          password: password
       );
+      Navigator.of(_context).popAndPushNamed("/homeview");
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
       }
-    } catch (e) {
-      print(e);
     }
   }
 
