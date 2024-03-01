08685e4a818e667105d3b6bd3fc3d686b54aac1d
Pablobel <pablo.beltran.reyfernando@gmail.com>
2024-03-01 10:20:54 +0100

    Creación e implementación de PostCellView

---
+++ lib/Custom/PostCellView.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PostCellView extends StatelessWidget {
+  final String text;
+  final int colorCode;
+  final double fontSize;
+
+  const PostCellView(
+      {super.key,
+      required this.text,
+      required this.colorCode,
+      required this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.amber[colorCode],
+        child: Row(
+          children: [
+            Image.asset("resources/cambiame1.png", width: 30, height: 45),
+            Text(text)
+          ],
+        ));
+  }
+}
--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,8 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:kytypablo/Custom/PostCellView.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  final List<String> post = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+  final List<double> fontSize = <double>[30, 15, 70];
+
+  Widget? creadorDeItemLista(BuildContext context, int index) {
+    return PostCellView(
+        text: post[index],
+        colorCode: colorCodes[index],
+        fontSize: fontSize[index]);
+  }
+
+  Widget creadorDeSeparadorLista(BuildContext context,int index){
+    return Column(
+      children:[
+        Divider(),
+        //Imagen
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("HOLA A HOME");
+    return Scaffold(
+      appBar: AppBar(title: Text("FRIKEANDO")),
+      body: ListView.separated(
+        padding: EdgeInsets.all(80),
+        itemCount: post.length,
+        itemBuilder: creadorDeItemLista,
+        separatorBuilder:creadorDeSeparadorLista,
+      ),
+    );
   }
 }
