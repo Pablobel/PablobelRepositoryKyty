a444d6ffd65f33d85260940ee982513e838624aa
Pablobel <pablo.beltran.reyfernando@gmail.com>
2024-06-10 00:06:11 +0200

    Splash mejorado.

--- lib/Splash/SplashView.dart
+++ lib/Splash/SplashView.dart
@@ -53,19 +53,24 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
-    Column columna = Column(
-      children: [
-        Image.asset(
-          "resources/tienda.png",
-          width: DataHolder().platformAdmin.getScreenWidth()*0.4,
-          height: DataHolder().platformAdmin.getScreenHeight()*0.4,
-        ),
-        CircularProgressIndicator()
-      ],
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "resources/tienda.png",
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
-
-    //return Image.network("https://cdn.discordapp.com/attachments/1094893510609608764/1154156474763845703/yony44_logo_using_cat_ears_and_head_like_disney_symbol_happy_an_1fccd304-c37e-410f-a9bd-155c310e365a.png?ex=65e8d5fa&is=65d660fa&hm=b175fa097f6fb91c9c934125f13260ec643ed9304249b6aa1b77fbb86c0513ac&.png");
-    //return Image.asset("resources/cambiame2.png");
-    return columna;
   }
 }
